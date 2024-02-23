d1adfb9b3140f7ae4b5963ac56aac01ac9a2ef5c
Emilorzz <emil69h@gmail.com>
2024-02-23 13:38:25 +0100

    added properties to NTA class

--- TimedRegex/XmlGenerator/NTA.cs
+++ TimedRegex/XmlGenerator/NTA.cs
@@ -2,5 +2,7 @@ namespace TimedRegex;
 
 public class NTA
 {
-    
+    public string Declaration { get; set; }
+    public string System { get; set; }
+    public List<Template> Templates { get; set; }
 }
