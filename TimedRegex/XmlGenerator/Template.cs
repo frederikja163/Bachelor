1399bf157386f8af249dd883a0a971e140687c09
Emilorzz <emil69h@gmail.com>
2024-02-23 13:38:48 +0100

    added properties to Template class

--- TimedRegex/XmlGenerator/Template.cs
+++ TimedRegex/XmlGenerator/Template.cs
@@ -2,5 +2,9 @@ namespace TimedRegex;
 
 public class Template
 {
-    
+    public string Declaration { get; set; }
+    public string name { get; set; }
+    public string init { get; set; }
+    public List<Location> Locations { get; set; }
+    public List<Transition> Transitions { get; set; }
 }
