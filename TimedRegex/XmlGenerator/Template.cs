ac2af0762932f79ee0115178c38740a60fc5f797
Emilorzz <emil69h@gmail.com>
2024-02-23 14:26:08 +0100

    small changes

--- TimedRegex/XmlGenerator/Template.cs
+++ TimedRegex/XmlGenerator/Template.cs
@@ -4,6 +4,7 @@ public class Template
 {
     public string Declaration { get; set; }
     public string name { get; set; }
+    // init must be set to id that exists.
     public string init { get; set; }
     public List<Location> Locations { get; set; }
     public List<Transition> Transitions { get; set; }
--- TimedRegex/XmlGenerator/Transition.cs
+++ TimedRegex/XmlGenerator/Transition.cs
@@ -5,5 +5,5 @@ public class Transition
     public string Id { get; set; }
     public string Source { get; set; }
     public string Target { get; set; }
-    public List<(string, string)> Labels { get; set; }
+    public List<(string Kind, string Label)> Labels { get; set; }
 }
