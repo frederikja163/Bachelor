4832788a9a24690881ac4cca909f78c1bb59eb88
Emilorzz <emil69h@gmail.com>
2024-02-23 13:42:41 +0100

    added properties for Transition class

--- TimedRegex/XmlGenerator/Transition.cs
+++ TimedRegex/XmlGenerator/Transition.cs
@@ -2,5 +2,8 @@ namespace TimedRegex;
 
 public class Transition
 {
-    
+    public string Id { get; set; }
+    public string Source { get; set; }
+    public string Target { get; set; }
+    public List<(string, string)> Labels { get; set; }
 }
