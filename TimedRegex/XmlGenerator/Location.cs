22281bb1aa32b4d8d572e9efed748ea2dd485a84
Emilorzz <emil69h@gmail.com>
2024-02-23 13:41:06 +0100

    added properties to Location class

--- TimedRegex/XmlGenerator/Location.cs
+++ TimedRegex/XmlGenerator/Location.cs
@@ -2,5 +2,7 @@ namespace TimedRegex;
 
 public class Location
 {
-    
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public List<(string, string)> Labels { get; set; }
 }
