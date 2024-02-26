9e08ad484bc95f9f1b72eb7169fdcc81c04e149e
FrederikJA <fjaplv64@gmail.com>
2024-02-26 09:19:11 +0100

    Use config from main

---
+++ TimedRegex.Test/CliTest.cs
@@ -0,0 +1,6 @@
+namespace TimedRegex.Test;
+
+public class CliTest
+{
+    
+}
--- TimedRegex/Program.cs
+++ TimedRegex/Program.cs
@@ -4,6 +4,6 @@ internal static class Program
 {
     internal static void Main(string[] args)
     {
-        
+        Config config = new Config(args);
     }
 }
