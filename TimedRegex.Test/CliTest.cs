3802c8d32ec31d611b4b2ea4a7734a01062244c7
FrederikJA <fjaplv64@gmail.com>
2024-02-26 09:31:37 +0100

    make test public

--- TimedRegex.Test/CliTest.cs
+++ TimedRegex.Test/CliTest.cs
@@ -2,11 +2,11 @@ using NUnit.Framework;
 
 namespace TimedRegex.Test;
 
-internal sealed class CliTest
+public sealed class CliTest
 {
     [TestCase("TRegex", "-h", "TRegex")]
     [TestCase("TRegex", "TRegex")]
-    internal void GetRegexFromConfigTest(string expected, params string[] args)
+    public void GetRegexFromConfigTest(string expected, params string[] args)
     {
         Config config = new Config(args);
         Assert.That(expected, Is.EqualTo(config.TimeRegex));
