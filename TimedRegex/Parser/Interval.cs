eb4308cc1ce92b764028b826dc276408e90cff87
IronicJuice <81492870+IronicJuice@users.noreply.github.com>
2024-02-27 11:04:04 +0100

    Started implementing IAstNode

--- TimedRegex/Parser/Interval.cs
+++ TimedRegex/Parser/Interval.cs
@@ -2,6 +2,10 @@
 {
     internal class Interval : Unary
     {
+        internal int startInterval;
+        internal int endInterval;
+        internal bool startInclusive;
+        internal bool endInclusive;
 
     }
 }
--- TimedRegex/Parser/Match.cs
+++ TimedRegex/Parser/Match.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TimedRegex.Parser
+﻿namespace TimedRegex.Parser
 {
-    internal class Match
+    internal class Match :IAstNode
     {
+        internal string token;
     }
 }
