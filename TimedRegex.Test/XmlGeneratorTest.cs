86a0579657d3204e3440974a5192ee315297cf12
Emilorzz <emil69h@gmail.com>
2024-03-04 14:22:57 +0100

    added assert for checking that xml file equals an expected string. this might be a trivial test

--- TimedRegex.Test/XmlGeneratorTest.cs
+++ TimedRegex.Test/XmlGeneratorTest.cs
@@ -33,6 +33,8 @@ public sealed class XmlGeneratorTest
         XmlGenerator xmlGenerator = new XmlGenerator();
         NTA nta = CreateNta();
 
+        string expected =
+            "<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<nta>\r\n  <declaration>clock a, b;</declaration>\r\n  <template>\r\n    <name>ta1</name>\r\n    <location id=\"id0\" />\r\n    <location id=\"id1\" />\r\n    <location id=\"id2\" />\r\n    <location id=\"id3\" />\r\n    <location id=\"id4\" />\r\n    <init ref=\"id0\" />\r\n    <transition ref=\"id5\">\r\n      <source ref=\"id0\" />\r\n      <target ref=\"id1\" />\r\n    </transition>\r\n    <transition ref=\"id6\">\r\n      <source ref=\"id0\" />\r\n      <target ref=\"id2\" />\r\n    </transition>\r\n    <transition ref=\"id7\">\r\n      <source ref=\"id1\" />\r\n      <target ref=\"id3\" />\r\n      <label kind=\"guard\">1 &lt;= A &lt; 5</label>\r\n    </transition>\r\n    <transition ref=\"id8\">\r\n      <source ref=\"id2\" />\r\n      <target ref=\"id4\" />\r\n      <label kind=\"guard\">1 &lt;= B &lt; 3</label>\r\n    </transition>\r\n    <system>system ta1</system>\r\n  </template>\r\n</nta>";
         StringBuilder sb = new StringBuilder();
 
         XmlWriterSettings settings = new() { Indent = true };
@@ -42,6 +44,7 @@ public sealed class XmlGeneratorTest
         }
 
         Assert.That(sb.ToString(), Is.Not.EqualTo(""));
+        Assert.That(sb.ToString(), Is.EqualTo(expected));
     }
 
     [Test]
