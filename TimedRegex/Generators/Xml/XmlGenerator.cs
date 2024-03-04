1a2e5da1e9c32973ccd8743a04d5a3fd9dd32e0b
Emilorzz <emil69h@gmail.com>
2024-03-04 14:15:29 +0100

    separated method WriteInfo into smaller methods for more exhaustive testing

--- TimedRegex/Generators/Xml/XmlGenerator.cs
+++ TimedRegex/Generators/Xml/XmlGenerator.cs
@@ -18,70 +18,88 @@ internal sealed class XmlGenerator : IGenerator
         XmlWriterSettings settings = new() { Indent = true };
         using XmlWriter xmlWriter = XmlWriter.Create(stream, settings);
 
-        WriteInfo(xmlWriter, nta);
+        xmlWriter.WriteStartDocument();
+        WriteNta(xmlWriter, nta);
     }
 
-    internal void WriteInfo(XmlWriter xmlWriter, NTA nta)
+    internal void WriteNta(XmlWriter xmlWriter, NTA nta)
     {
-        xmlWriter.WriteStartDocument();
-
         xmlWriter.WriteStartElement("nta");
-        if (!String.IsNullOrWhiteSpace(nta.Declaration))
-        {
-            xmlWriter.WriteStartElement("declaration");
-            xmlWriter.WriteValue(nta.Declaration);
-            xmlWriter.WriteEndElement();
-        }
-
+        
+        if (String.IsNullOrWhiteSpace(nta.Declaration)) return;
+        xmlWriter.WriteStartElement("declaration");
+        xmlWriter.WriteValue(nta.Declaration);
+        xmlWriter.WriteEndElement();
+        
         foreach (var template in nta.Templates)
         {
-            xmlWriter.WriteStartElement("template");
-            xmlWriter.WriteStartElement("name");
-            xmlWriter.WriteValue(template.Name);
-            xmlWriter.WriteEndElement();
-
-            foreach (var location in template.Locations)
-            {
-                xmlWriter.WriteStartElement("location");
-                xmlWriter.WriteAttributeString("id", location.Id);
-                //if we want to account for location names, and name differs from id
-                //xmlWriter.WriteValue(location.Name);
-                xmlWriter.WriteEndElement();
-            }
-
-            xmlWriter.WriteStartElement("init");
-            xmlWriter.WriteAttributeString("ref", template.Init);
-            xmlWriter.WriteEndElement();
-
-            foreach (var transition in template.Transitions)
-            {
-                xmlWriter.WriteStartElement("transition");
-                xmlWriter.WriteAttributeString("ref", transition.Id);
-
-                xmlWriter.WriteStartElement("source");
-                xmlWriter.WriteAttributeString("ref", transition.Source);
-                xmlWriter.WriteEndElement();
-
-                xmlWriter.WriteStartElement("target");
-                xmlWriter.WriteAttributeString("ref", transition.Target);
-                xmlWriter.WriteEndElement();
-
-                foreach (var label in transition.Labels)
-                {
-                    xmlWriter.WriteStartElement("label");
-                    xmlWriter.WriteAttributeString("kind", "guard");
-                    xmlWriter.WriteValue(label.LabelString);
-                    xmlWriter.WriteEndElement();
-                }
-
-                xmlWriter.WriteEndElement();
-            }
+            WriteTemplate(xmlWriter, template);
         }
 
         xmlWriter.WriteStartElement("system");
         xmlWriter.WriteValue(nta.System);
         xmlWriter.WriteEndElement();
 
+        xmlWriter.WriteEndElement();
+    }
+
+    internal void WriteTemplate(XmlWriter xmlWriter, Template template)
+    {
+        xmlWriter.WriteStartElement("template");
+        xmlWriter.WriteStartElement("name");
+        xmlWriter.WriteValue(template.Name);
+        xmlWriter.WriteEndElement();
+
+        foreach (var location in template.Locations)
+        {
+            WriteLocation(xmlWriter, location);
+        }
+
+        xmlWriter.WriteStartElement("init");
+        xmlWriter.WriteAttributeString("ref", template.Init);
+        xmlWriter.WriteEndElement();
+
+        foreach (var transition in template.Transitions)
+        {
+            WriteTransition(xmlWriter, transition);
+        }
+    }
+
+    internal void WriteLocation(XmlWriter xmlWriter, Location location)
+    {
+        xmlWriter.WriteStartElement("location");
+        xmlWriter.WriteAttributeString("id", location.Id);
+        //if we want to account for location names, and name differs from id
+        //xmlWriter.WriteValue(location.Name);
+        xmlWriter.WriteEndElement();
+    }
+
+    internal void WriteTransition(XmlWriter xmlWriter, Transition transition)
+    {
+        xmlWriter.WriteStartElement("transition");
+        xmlWriter.WriteAttributeString("ref", transition.Id);
+
+        xmlWriter.WriteStartElement("source");
+        xmlWriter.WriteAttributeString("ref", transition.Source);
+        xmlWriter.WriteEndElement();
+
+        xmlWriter.WriteStartElement("target");
+        xmlWriter.WriteAttributeString("ref", transition.Target);
+        xmlWriter.WriteEndElement();
+
+        foreach (var label in transition.Labels)
+        {
+            WriteLabel(xmlWriter, label);
+        }
+
+        xmlWriter.WriteEndElement();
+    }
+
+    internal void WriteLabel(XmlWriter xmlWriter, Label label)
+    {
+        xmlWriter.WriteStartElement("label");
+        xmlWriter.WriteAttributeString("kind", "guard");
+        xmlWriter.WriteValue(label.LabelString);
         xmlWriter.WriteEndElement();
     }
 
