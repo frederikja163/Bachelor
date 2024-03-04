7a0f136708c8959c3be21a6ea45675ddfb377725
Emilorzz <emil69h@gmail.com>
2024-03-04 11:13:54 +0100

    Created method WriteInfo for writing to xml file

--- TimedRegex/Generators/Xml/XmlGenerator.cs
+++ TimedRegex/Generators/Xml/XmlGenerator.cs
@@ -1,3 +1,4 @@
+using System.Xml;
 using TimedRegex.Intermediate;
 
 namespace TimedRegex.Generators.Xml;
@@ -11,19 +12,82 @@ internal sealed class XmlGenerator : IGenerator
 
     public void GenerateFile(Stream stream, TimedAutomaton automaton)
     {
-        throw new NotImplementedException();
+        // Empty NTA is temporary, missing implementation of NTA instantiation
+        NTA nta = new NTA("", "", Enumerable.Empty<Template>());
+
+        XmlWriterSettings settings = new() { Indent = true };
+        using XmlWriter xmlWriter = XmlWriter.Create(stream, settings);
+
+        WriteInfo(xmlWriter, nta);
+    }
+
+    private void WriteInfo(XmlWriter xmlWriter, NTA nta)
+    {
+        xmlWriter.WriteStartDocument();
+
+        xmlWriter.WriteStartElement("nta");
+        if (!String.IsNullOrWhiteSpace(nta.Declaration))
+        {
+            xmlWriter.WriteStartElement("declaration");
+            xmlWriter.WriteValue(nta.Declaration);
+            xmlWriter.WriteEndElement();
+        }
+
+        foreach (var template in nta.Templates)
+        {
+            xmlWriter.WriteStartElement("template");
+            xmlWriter.WriteStartElement("name");
+            xmlWriter.WriteValue(template.Name);
+
+            foreach (var location in template.Locations)
+            {
+                xmlWriter.WriteStartElement("location");
+                xmlWriter.WriteAttributeString("id", location.Id);
+                //if we want to account for location names, and name differs from id
+                //xmlWriter.WriteValue(location.Name);
+                xmlWriter.WriteEndElement();
+            }
+
+            xmlWriter.WriteStartElement("init");
+            xmlWriter.WriteAttributeString("ref", template.Init);
+            xmlWriter.WriteEndElement();
+
+            foreach (var transition in template.Transitions)
+            {
+                xmlWriter.WriteStartElement("transition");
+                xmlWriter.WriteAttributeString("ref", transition.Id);
+
+                xmlWriter.WriteStartElement("source");
+                xmlWriter.WriteAttributeString("ref", transition.Source);
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteStartElement("target");
+                xmlWriter.WriteAttributeString("ref", transition.Target);
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteEndElement();
+            }
+
+            xmlWriter.WriteEndElement();
+        }
+
+        xmlWriter.WriteStartElement("system");
+        xmlWriter.WriteValue(nta.System);
+        xmlWriter.WriteEndElement();
+
+        xmlWriter.WriteEndElement();
     }
 
     private void PopulateNta(TimedAutomaton automaton)
     {
         throw new NotImplementedException();
     }
-    
+
     private void PopulateTemplate(TimedAutomaton automaton)
     {
         throw new NotImplementedException();
     }
-    
+
     private void PopulateLocation(TimedAutomaton automaton)
     {
         throw new NotImplementedException();
