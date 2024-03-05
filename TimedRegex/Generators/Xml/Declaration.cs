70829bcff3cf2cd5e6d4e83307a0df48397251da
Emilorzz <emil69h@gmail.com>
2024-03-05 09:24:17 +0100

    added properties to declaration class

--- TimedRegex/Generators/Xml/Declaration.cs
+++ TimedRegex/Generators/Xml/Declaration.cs
@@ -4,12 +4,12 @@ namespace TimedRegex.Generators.Xml;
 
 internal sealed class Declaration
 {
-    private readonly List<Clock> _clocks;
-    private readonly List<string> _channels;
-    
     public Declaration (List<Clock> clocks, List<string> channels)
     {
-        _clocks = clocks;
-        _channels = channels;
+        Clocks = clocks;
+        Channels = channels;
     }
+    
+    internal List<Clock> Clocks { get; }
+    internal List<string> Channels { get; }
 }
