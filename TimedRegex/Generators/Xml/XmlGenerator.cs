94b21cb55de9349d5b1135456b7da21c4c29ac62
Emilorzz <emil69h@gmail.com>
2024-03-12 14:39:15 +0100

    removed comment

--- TimedRegex/Generators/Xml/XmlGenerator.cs
+++ TimedRegex/Generators/Xml/XmlGenerator.cs
@@ -50,7 +50,6 @@ internal sealed class XmlGenerator : IGenerator
     private Declaration GenerateDeclaration(TimedAutomaton timedAutomaton)
     {
         IEnumerable<string> clocks = timedAutomaton.GetClocks().Select(clocks => "c" + clocks.Id).ToList();
-        //IEnumerable<string> channels = timedAutomaton.GetAlphabet().ToList().Where(x => x != '\0');
         IEnumerable<string> channels = timedAutomaton.GetAlphabet()
             .Where(x => x != '\0')
             .Select(s => s.ToString())
