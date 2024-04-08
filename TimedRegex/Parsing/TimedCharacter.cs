fe85de63ea83d0b37d39808b47f877b9fc846cba
IronicJuice <81492870+IronicJuice@users.noreply.github.com>
2024-04-08 11:40:26 +0200

    Implemented TimedWord.GetStringFromCSV

--- TimedRegex/Parsing/TimedCharacter.cs
+++ TimedRegex/Parsing/TimedCharacter.cs
@@ -1,16 +1,45 @@
-﻿namespace TimedRegex.Parsing;
+﻿using CommandLine;
 
-internal sealed class TimedCharacter
+namespace TimedRegex.Parsing;
+
+internal sealed class TimedCharacter : IEquatable<TimedCharacter>
 {
     private readonly char _char;
-    private readonly int _time;
+    private readonly float _time;
 
-    public TimedCharacter(char symbol, int time)
+    public TimedCharacter(string s)
+    {
+        string[] strArr = s.Split(',');
+        if (strArr.Length != 2)
+        {
+            throw new FormatException("Tried to create TimedCharacter from string array with invalid length.");
+        }
+        if (strArr[0].Length != 1)
+        {
+            throw new FormatException("Time characters can not be more than 1 character long.");
+        }
+        _char = strArr[0][0];
+        if (!float.TryParse(strArr[1], out _time))
+        {
+            throw new FormatException("Could not parse float from " + strArr[1]);
+        }
+    }
+
+    public TimedCharacter(char symbol, float time)
     {
         _char = symbol;
         _time = time;
     }
 
     public char Char => _char;
-    public int Time => _time;
+    public float Time => _time;
+
+    public bool Equals(TimedCharacter? other)
+    {
+        if (other == null)
+        {
+            return false;
+        }
+        return (_char == other._char && _time == other._time);
+    }
 }
