d7d8eb2c523c2347cae33b8291d3c2ec4bacfd00
FrederikJA <fjaplv64@gmail.com>
2024-05-21 18:45:20 +0200

    New benchmarks

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -1,4 +1,6 @@
-﻿using Benchmark;
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+using Benchmark;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 using TimedRegex.AST;
@@ -9,71 +11,97 @@ internal static class Program
 {
     internal static void Main(string[] args)
     {
-        BenchmarkRunner.Run<Benchmarks>();
+        BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
     }
 }
 
 [MemoryDiagnoser]
 public class Benchmarks
 {
-    private static readonly IAstNode Root;
-    
-    static Benchmarks()
+    public IEnumerable<object[]> ArgumentSource()
     {
-        string regex = string.Join("+'", Enumerable.Repeat("(J[5;10]&J[3;5])", 10));
-        Console.WriteLine(regex);
-        Root = Parser.Parse(new Tokenizer(regex));
+        return Regexes().Select(r => new object[]{r, Parser.Parse(new Tokenizer(r))});
+        
+        IEnumerable<string> Regexes()
+        {
+            // yield return RepeatNested(1, "(", "J", ")+'");
+            // yield return RepeatNested(2, "(", "J", ")+'");
+            // yield return RepeatNested(4, "(", "J", ")+'");
+            // yield return RepeatNested(1, "(", "J[1;1]", ")+'");
+            // yield return RepeatNested(2, "(", "J[1;1]", ")+'");
+            // yield return RepeatNested(4, "(", "J[1;1]", ")+'");
+            yield return string.Join("", Enumerable.Repeat("J[1;10]", 500)) + "&J[1;1]";
+            yield return string.Join("", Enumerable.Repeat("J", 500)) + "&J";
+            yield return string.Join("", Enumerable.Repeat("J", 500)) + "+'";
+            // yield return RepeatNested(8, "(", "J", ")+'");
+            // yield return string.Join("&", Enumerable.Repeat("J[1;1]", 1));
+            // yield return string.Join("&", Enumerable.Repeat("J[1;1]", 2));
+            // yield return string.Join("&", Enumerable.Repeat("J[1;1]", 4));
+            // yield return string.Join("&", Enumerable.Repeat("J[1;1]", 8));
+            // yield return string.Join("&", Enumerable.Repeat("J[1;1]", 16));
+        }
+
+        string RepeatNested(int count, string prefix, string middle, string end)
+        {
+            return string.Join("", Enumerable.Repeat(prefix, count).Append(middle).Concat(Enumerable.Repeat(end, count)));
+        }
     }
     
     [Benchmark]
-    public void NoPruning()
+    [ArgumentsSource(nameof(ArgumentSource))]
+    public void NoPruning(string regex, object root)
     {
         var visitor = new Standard();
-        Root.Accept(visitor);
+        ((IAstNode)root).Accept(visitor);
         TimedAutomaton ta = visitor.GetAutomaton();
     }
     
     //[Benchmark]
-    public void AllPrunings()
+    [ArgumentsSource(nameof(ArgumentSource))]
+    public void AllPrunings(string regex, object root)
     {
         var visitor = new Standard();
-        Root.Accept(visitor);
+        ((IAstNode)root).Accept(visitor);
         TimedAutomaton ta = visitor.GetAutomaton();
         ta.PruneEverything();
     }
     
-    [Benchmark]
-    public void StatePruning()
+    //[Benchmark]
+    [ArgumentsSource(nameof(ArgumentSource))]
+    public void StatePruning(string regex, object root)
     {
         var visitor = new Standard();
-        Root.Accept(visitor);
+        ((IAstNode)root).Accept(visitor);
         TimedAutomaton ta = visitor.GetAutomaton();
         ta.PruneStates();
     }
     
     [Benchmark]
-    public void PreOpStatePruning()
+    [ArgumentsSource(nameof(ArgumentSource))]
+    public void PreOpStatePruningAndState(string regex, object root)
     {
         var visitor = new PreOpStatePruning();
-        Root.Accept(visitor);
+        ((IAstNode)root).Accept(visitor);
         TimedAutomaton ta = visitor.GetAutomaton();
         ta.PruneStates();
     }
     
-    [Benchmark]
-    public void PreOpStatePruningAndAll()
+    //[Benchmark]
+    [ArgumentsSource(nameof(ArgumentSource))]
+    public void PreOpStatePruningAndAll(string regex, object root)
     {
         var visitor = new PreOpStatePruning();
-        Root.Accept(visitor);
+        ((IAstNode)root).Accept(visitor);
         TimedAutomaton ta = visitor.GetAutomaton();
         ta.PruneEverything();
     }
     
-    [Benchmark]
-    public void PreOpAllPruning()
+    //[Benchmark]
+    [ArgumentsSource(nameof(ArgumentSource))]
+    public void PreOpAllPruningAndState(string regex, object root)
     {
         var visitor = new PreOpAllPruning();
-        Root.Accept(visitor);
+        ((IAstNode)root).Accept(visitor);
         TimedAutomaton ta = visitor.GetAutomaton();
         ta.PruneStates();
     }
